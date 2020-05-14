335b69bea8111c7093fbbe5565356c954e6304d6
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-05-14 17:47:06 +0900

    ffmpeg-pythonでMP4の動画をMP3に変換するとこまで

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import sys
 import logging
 import logzero
 from logzero import logger
 import click
 from pytube import YouTube
 import os
+import ffmpeg
+import re
 
 DOWNLOAD_DIR = 'downloaded/'
 LOG_DIR = 'log' + os.sep
@@ -30,19 +33,34 @@ def main(youtube_id, debug, convert_mp3):
         yt = YouTube('https://www.youtube.com/watch?v=' + youtube_id)
     except:
         logger.error("指定のIDの動画は見つかりませんでした")
-        return
+        sys.exit(1)
 
     # 解像度の高い順でStreamを取得
-    stream = yt.streams.filter(progressive=True,mime_type='video/mp4').order_by('resolution').desc().first()
+    stream = yt.streams.filter(progressive=True, mime_type='video/mp4').order_by('resolution').desc().first()
     if stream is None:
         logger.error("mp4形式の動画が見つかりませんでした。")
-        return
+        sys.exit(1)
 
-    logger.info("動画が見つかりました。ファイル名：{0}".format(stream.title))
-    logger.debug(stream)
+    logger.info("動画が見つかりました。タイトル：{0}".format(stream.title))
     stream.download(DOWNLOAD_DIR, stream.title)
     logger.info("動画の保存に成功しました。")
+    # ファイル名として使えない文字を除いた文字列を作成
+    stream_title = re.sub(r'[\\/:*?"<>|]+', '', stream.title)
+    mp4_file_name = stream_title + '.mp4'
 
+    if convert_mp3:
+        mp3_file_name = mp4_file_name.replace('.mp4', '.mp3')
+        logger.info("動画をMP3に変換します。ファイル名：{0}".format(mp3_file_name))
+        try:
+            ffmpeg_stream = ffmpeg.input(DOWNLOAD_DIR + mp4_file_name)
+            logger.debug(ffmpeg_stream)
+            logger.debug(type(ffmpeg_stream))
+            ffmpeg_stream = ffmpeg.output(ffmpeg_stream, DOWNLOAD_DIR + mp3_file_name)
+            ffmpeg.run(ffmpeg_stream, overwrite_output=True)
+            logger.info("MP3の作成に成功しました。")
+        except:
+            logger.error("MP3の作成に失敗しました。")
+            sys.exit(1)
 
 if __name__ == "__main__":
     main()
