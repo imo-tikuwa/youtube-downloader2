5195d58ac54436db86b681a2bfb55370f30485ca
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-05-27 08:51:29 +0900

    動画ダウンロード済み、MP3作成済みのファイルについて再生成の確認処理を追加、他
    動画のダウンロード処理とMP3の生成処理を関数化
    ID3タグに埋め込むサムネイルについて動画から切り出す際の秒数を指定できるようオプション追加

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ import os
 import ffmpeg
 import re
 import eyed3
+import json
 
-DOWNLOAD_DIR = 'downloaded/'
+DOWNLOAD_DIR = 'downloaded' + os.sep
+DOWNLOADED_LOG_FILE = DOWNLOAD_DIR + '.json'
 LOG_DIR = 'log' + os.sep
 LOG_FILE = LOG_DIR + 'application.log'
 
@@ -20,56 +22,88 @@ if not os.path.exists(LOG_DIR):
 logzero.logfile(LOG_FILE, encoding = "utf-8")
 logzero.loglevel(logging.INFO)
 
-@click.command()
-@click.option('--youtube-id', '-yid', required = True, help = 'youtubeの動画ID(必須)')
-@click.option('--debug', is_flag = True, help = "debugログを出力します")
-@click.option('--convert-mp3', is_flag = True, help = "mp3に変換して出力します")
-def main(youtube_id, debug, convert_mp3):
+# ダウンロード済みファイルの辞書
+if not os.path.exists(DOWNLOADED_LOG_FILE):
+    json.dump({}, open(DOWNLOADED_LOG_FILE, 'w'), indent=4)
+downloaded_dict = json.load(open(DOWNLOADED_LOG_FILE, encoding='unicode-escape'))
 
-    if debug:
-        logzero.loglevel(logging.DEBUG)
-        logger.debug("youtube_id:{0}".format(youtube_id))
 
+def download_youtube_movie(youtube_id):
+    """
+    Youtubeから引数のIDを元に動画をダウンロードする
+    """
     try:
         yt = YouTube('https://www.youtube.com/watch?v=' + youtube_id)
     except:
         logger.error("指定のIDの動画は見つかりませんでした")
-        sys.exit(1)
+        return False, None
 
     # 解像度の高い順でStreamを取得
     stream = yt.streams.filter(progressive=True, mime_type='video/mp4').order_by('resolution').desc().first()
     if stream is None:
         logger.error("mp4形式の動画が見つかりませんでした。")
-        sys.exit(1)
+        return False, None
 
     logger.info("動画が見つかりました。タイトル：{0}".format(stream.title))
     stream.download(DOWNLOAD_DIR, stream.title)
     logger.info("動画の保存に成功しました。")
     # ファイル名として使えない文字を除いた文字列を作成
     stream_title = re.sub(r'[\\/:*?"<>|]+', '', stream.title)
-    mp4_file_name = stream_title + '.mp4'
+
+    return True, stream_title
+
+
+@click.command()
+@click.option('--youtube-id', '-yid', required = True, help = 'youtubeの動画ID(必須)')
+@click.option('--debug', is_flag = True, help = "debugログを出力します")
+@click.option('--convert-mp3', is_flag = True, help = "mp3に変換して出力します")
+@click.option('--thumb-second', required = False, help = 'サムネイル作成対象とする秒数を指定', type = int, default = 1)
+def main(youtube_id, debug, convert_mp3, thumb_second):
+
+    if debug:
+        logzero.loglevel(logging.DEBUG)
+        logger.debug("youtube_id:{0}".format(youtube_id))
+
+    download_flag = True
+    if youtube_id in downloaded_dict and os.path.exists(DOWNLOAD_DIR + downloaded_dict[youtube_id] + '.mp4'):
+        print('入力した動画IDの動画は既にダウンロード済みのようです。再度ダウンロードしますか？[y/N]')
+        if input() not in ['Y', 'y']:
+            download_flag = False
+
+    if download_flag:
+        download_result, stream_title = download_youtube_movie(youtube_id)
+        if not download_result:
+            sys.exit(1)
+
+        # jsonにダウンロード情報を記録
+        downloaded_dict[youtube_id] = stream_title
+        json.dump(downloaded_dict, open(DOWNLOADED_LOG_FILE, 'w'), indent=4)
+    else:
+        # jsonから動画のタイトルを取得
+        stream_title = downloaded_dict[youtube_id]
 
     if convert_mp3:
-        mp3_file_name = mp4_file_name.replace('.mp4', '.mp3')
-        logger.info("動画をMP3に変換します。ファイル名：{0}".format(mp3_file_name))
+        if youtube_id in downloaded_dict and os.path.exists(DOWNLOAD_DIR + downloaded_dict[youtube_id] + '.mp3'):
+            print('ダウンロードした動画に対応するMP3が既に存在します。再度作成しますか？[y/N]')
+            if input() not in ['Y', 'y']:
+                logger.info("MP3の作成をやめたためプログラムを終了します。")
+                sys.exit(0)
+
+        logger.info("動画をMP3に変換します。ファイル名：{0}".format(stream_title + '.mp3'))
         try:
-            ffmpeg_stream = ffmpeg.input(DOWNLOAD_DIR + mp4_file_name)
-            logger.debug(ffmpeg_stream)
-            logger.debug(type(ffmpeg_stream))
-            ffmpeg_stream = ffmpeg.output(ffmpeg_stream, DOWNLOAD_DIR + mp3_file_name)
-            ffmpeg.run(ffmpeg_stream, overwrite_output=True)
+            ffmpeg_stream = ffmpeg.input(DOWNLOAD_DIR + stream_title + '.mp4')
+            ffmpeg_stream = ffmpeg.output(ffmpeg_stream, DOWNLOAD_DIR + stream_title + '.mp3')
+            ffmpeg.run(ffmpeg_stream, overwrite_output=True, quiet=True)
 
             logger.info("動画からサムネイルを生成してMP3のID3タグに設定")
             try:
-                thumb_file_name = mp4_file_name.replace('.mp4', '.png')
-                ffmpeg.input(DOWNLOAD_DIR + mp4_file_name, ss=1).output(DOWNLOAD_DIR + thumb_file_name, vframes=1).run(overwrite_output=True)
+                ffmpeg.input(DOWNLOAD_DIR + stream_title + '.mp4', ss=1).output(DOWNLOAD_DIR + stream_title + '.png', vframes=1, ss=thumb_second).run(overwrite_output=True, quiet=True)
                 logger.info("サムネイル生成に成功")
 
-                mp3_id3 = eyed3.load(DOWNLOAD_DIR + mp3_file_name)
+                mp3_id3 = eyed3.load(DOWNLOAD_DIR + stream_title + '.mp3')
                 mp3_id3.initTag()
-                mp3_id3.tag.title = stream.title
-                mp3_id3.tag.images.set(eyed3.id3.frames.ImageFrame.FRONT_COVER, open(DOWNLOAD_DIR + thumb_file_name, 'rb').read(), 'image/png')
-                logger.debug(mp3_id3)
+                mp3_id3.tag.title = stream_title
+                mp3_id3.tag.images.set(eyed3.id3.frames.ImageFrame.FRONT_COVER, open(DOWNLOAD_DIR + stream_title + '.png', 'rb').read(), 'image/png')
                 mp3_id3.tag.save(encoding='utf-8', version=eyed3.id3.ID3_V2_3)
                 logger.info("サムネイルの設定に成功")
             except:
